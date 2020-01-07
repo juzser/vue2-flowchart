7d99bcab47879e4a386d23422c5e14e8f62b90e0
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-07 12:29:32 +0700

    Change: style eslint for main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import VueFlowChart from './vue-flowchart'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(VueFlowChart)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
