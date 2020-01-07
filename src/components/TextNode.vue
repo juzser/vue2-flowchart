050d1b176e445269ac54ba1473ada4f7fe4e4a7a
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-07 12:23:34 +0700

    Add: text node component

---
+++ src/components/TextNode.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div Text Node
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    mainData: {
+      type: Object
+    }
+  }
+}
+</script>
