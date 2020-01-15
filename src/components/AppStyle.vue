a1bc40f96f8dd6f0156d034456ea9bf63b4c23a6
oe.sonnh <sonnh.90@gmail.com>
2020-01-15 09:25:45 +0700

    Add AppStyle

---
+++ src/components/AppStyle.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.qkfc-style(v-html="style", style="display: none")
+</template>
+<script>
+export default {
+  props: ['options'],
+  computed: {
+    style () {
+      const styles = `
+<style type="text/css" id="qkfc-style">
+
+</style>
+      `
+      return styles
+    }
+  }
+}
+</script>
