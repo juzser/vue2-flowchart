216d071c202d20e74b0af0fc4468e0e063ea8ded
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-16 15:40:24 +0700

    Add: options for vue flowchart

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template lang="pug">
 #app
-  VueFlowChart
+  VueFlowChart(
+    :nodes.sync="nodes"
+    :links.sync="links"
+    :options="options"
+    @linkDeleted='linkDeleted'
+    @nodeSelected='nodeSelected'
+    @nodeDeleted='nodeDeleted'
+    @linkSelected='linkSelected'
+  )
 </template>
 
 <script>
+import { nodes, links } from '@/helpers/fake-nodes'
+
 export default {
-  components: {}
+  data () {
+    return {
+      nodes: nodes,
+      links: links,
+      options: {
+        disableHoverLink: true
+      }
+    }
+  },
+  components: {},
+  methods: {
+    linkDeleted (id) {
+      console.log('linkDeleted: ' + id)
+    },
+    nodeSelected (id) {
+      console.log('nodeSelected: ' + id)
+    },
+    nodeDeleted (id) {
+      console.log('nodeDeleted: ' + id)
+    },
+    linkSelected (id) {
+      console.log('linkSelected: ' + id)
+    }
+  }
 }
 </script>
