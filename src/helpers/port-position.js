fdce952e4f33dec9bd136eeafd164a88f9e4c78b
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-16 15:43:36 +0700

    Fix: calculate port-position

--- src/helpers/port-position.js
+++ src/helpers/port-position.js
@@ -5,34 +5,30 @@ export const portPosition = {
   getPortLeft
 }
 
-// height, width of port
-const hp = 16
-const wp = 16
-
 function getPortTop (cx, cy, w, h) {
   let x, y
-  x = cx - wp / 2
-  y = cy - h / 2 - hp / 2
+  x = cx
+  y = cy - h / 2
   return [x, y]
 }
 
 function getPortRight (cx, cy, w, h) {
   let x, y
-  x = cx + w / 2 - wp / 2
-  y = cy - hp / 2
+  x = cx + w / 2
+  y = cy
   return [x, y]
 }
 
 function getPortBottom (cx, cy, w, h) {
   let x, y
-  x = cx - wp / 2
-  y = cy + h / 2 - hp / 2
+  x = cx
+  y = cy + h / 2
   return [x, y]
 }
 
 function getPortLeft (cx, cy, w, h) {
   let x, y
-  x = cx - w / 2 - wp / 2
-  y = cy - hp / 2
+  x = cx - w / 2
+  y = cy
   return [x, y]
 }
