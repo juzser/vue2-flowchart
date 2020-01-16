9f94cfd34896f27fc98b9e06a4911a2fd40fff90
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-16 15:42:09 +0700

    Change: Styles for flowchart

--- src/components/AppStyle.vue
+++ src/components/AppStyle.vue
@@ -6,8 +6,49 @@ export default {
   props: ['options'],
   computed: {
     style () {
+      if (!this.options) {
+        return ''
+      }
+
+      const {
+        nodeBdColor,
+        nodePortColor,
+        nodeBgColor,
+        nodePortSize,
+        iconDeleteNodeColor
+      } = this.options
+
       const styles = `
 <style type="text/css" id="qkfc-style">
+.qkfc-node {
+  background-color: ${nodeBgColor}
+}
+.qkfc-node--active {
+  border-top: 2.5px solid ${nodeBdColor};
+}
+
+qkfc-btn-node-option--active {
+  border-left: 2.5px solid ${nodeBgColor};
+}
+
+.qkfc-node-port {
+  border: 1.5px solid ${nodePortColor};
+  width: ${nodePortSize}px;
+  height: ${nodePortSize}px;
+  transform: translate(-${nodePortSize / 2 + 3}px, -${nodePortSize / 2 + 1}px);
+}
+
+.qkfc-node-port:hover {
+  background-color: ${nodePortColor}
+}
+
+.qkfc-btn-node-option--active {
+  border-left: 2px solid ${nodeBdColor}
+}
+
+.qkfc-node-btn-icon--delete-active {
+  fill: ${iconDeleteNodeColor};
+}
 
 </style>
       `
