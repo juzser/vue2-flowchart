200aab69de9c954434d2ae1f1ffb12d14b366ac2
Doan Duy Dat <doanduydat98@gmail.com>
2020-01-20 09:35:09 +0700

    Change: get port position

--- src/helpers/port-position.js
+++ src/helpers/port-position.js
@@ -1,3 +1,5 @@
+import { WIDTH_OF_NODE, HEIGHT_OF_TEXT_NODE, HEIGHT_OF_BUTTON_NODE } from '@/helpers/constant'
+
 export const portPosition = {
   getPortTop,
   getPortRight,
@@ -5,29 +7,45 @@ export const portPosition = {
   getPortLeft
 }
 
-function getPortTop (cx, cy, w, h) {
+function getHeight (nodeType) {
+  let height = 0
+  switch (nodeType) {
+    case 'button':
+      height = HEIGHT_OF_BUTTON_NODE
+      break
+    default:
+      height = HEIGHT_OF_TEXT_NODE
+  }
+  return height
+}
+
+function getPortTop (cx, cy, type) {
   let x, y
+  let h = getHeight(type)
   x = cx
   y = cy - h / 2
   return [x, y]
 }
 
-function getPortRight (cx, cy, w, h) {
+function getPortRight (cx, cy, type) {
   let x, y
+  let w = WIDTH_OF_NODE
   x = cx + w / 2
   y = cy
   return [x, y]
 }
 
-function getPortBottom (cx, cy, w, h) {
+function getPortBottom (cx, cy, type) {
   let x, y
+  let h = getHeight(type)
   x = cx
   y = cy + h / 2
   return [x, y]
 }
 
-function getPortLeft (cx, cy, w, h) {
+function getPortLeft (cx, cy, type) {
   let x, y
+  let w = WIDTH_OF_NODE
   x = cx - w / 2
   y = cy
   return [x, y]
